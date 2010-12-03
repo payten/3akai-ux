1ec2849c48a762cc5c140e862138bcd2cfa93166
andrewsears <andrew@andrewsears.com>
2010-12-03 14:34:54 -0500

    Got the pom version numbers to match with Nakamura Q1

--- dev/_configuration/config_custom.js
+++ dev/_configuration/config_custom.js
@@ -3,6 +3,7 @@ var sakai = sakai || {};
 sakai.config = sakai.config || {};
 
 // Insert custom configuration here
+sakai.config.L10N.DateFormat = "MM/dd/yyyy";
 
 // Custom CSS Files to load in
 sakai.config.skinCSS = ["/dev/_skins/nyu/nyu.skin.css"];
