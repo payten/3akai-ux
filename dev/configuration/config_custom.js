42ac382798c911d170ac1bea04ae94e4de00a865
Payten Giles <payten.giles@nyu.edu>
2011-06-09 12:47:06 +1000

    first attempt at nyu skin for new sakai ui (including ebook widget);

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -2,7 +2,7 @@ define(function() {
     // Insert custom configuration here
     var config = {
         // Custom CSS Files to load in
-        skinCSS : ["/dev/skins/default/skin.css"]
+        skinCSS : ["/dev/skins/nyu/nyu.skin.css"]
     };
     return config;
 });
