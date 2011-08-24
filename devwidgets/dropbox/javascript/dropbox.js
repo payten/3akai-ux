d60d65a391c33ee1fe13b433b25f1ce032f337a1
Payten Giles <payten.giles@gmail.com>
2011-08-24 10:46:23 +1000

    basic create and submit working

--- devwidgets/dropbox/javascript/dropbox.js
+++ devwidgets/dropbox/javascript/dropbox.js
@@ -77,15 +77,25 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         // Buttons
         $("#dropbox_btnInsertWidget", rootel).die("click");
         $("#dropbox_btnInsertWidget", rootel).live("click", function() {
-            sakai.api.Widgets.saveWidgetData(tuid, {}, function() {
-                sakai.api.Widgets.Container.informFinish(tuid, "dropbox"); 
+            var data = serializeFormToObject($("#dropbox_form", rootel));
+            data = $.extend(widgetData, data);
+            $.post(
+                dropboxWidgetPath + "?widgetid=" + tuid,
+                data,
+                function() {
+                    sakai.api.Widgets.Container.informFinish(tuid, "dropbox"); 
+                },
+                "json"
+            );
+            sakai.api.Widgets.saveWidgetData(tuid, data, function() {
+                // not using at the moment...
             });
             return false;
         });
         
         $("#dropbox_upload_form_btnSubmit", rootel).die("click");
         $("#dropbox_upload_form_btnSubmit", rootel).live("click", function() {
-            $("#dropbox_upload_form").attr("action", uploadPath);
+            $("#dropbox_upload_form").attr("action", dropboxSubmissionPath  + "?widgetid=" + tuid);
             $("#dropbox_upload_form").ajaxForm({
                 dataType: "json",
                 success: function(data){
@@ -124,6 +134,27 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                $("#dropbox_submissions ul", rootel).html("<li>"+widgetData.submissions[sakai.data.me.user.userid]+"</li>");
            }
         };
+        
+        /**
+         * Serialise a form and it's input values into a JSON object
+         * @param {element/selector} form - the form/element to serialise
+         * @return {object} the form data as a JSON object
+         */
+        var serializeFormToObject = function(form) {
+            var o = {};
+            var a = $(form).serializeArray();
+            $.each(a, function() {
+                if (o[this.name]) {
+                    if (!o[this.name].push) {
+                        o[this.name] = [o[this.name]];
+                    }
+                    o[this.name].push(this.value || '');
+                } else {
+                    o[this.name] = this.value || '';
+                }
+            });
+            return o;
+        };        
 
         ////////////////////////
         // Settings functions //
@@ -137,11 +168,13 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         var showSettingsScreen = function(response, exists) {
             if (exists) {
                 // Fill in dropbox info
-                
+                $("#dropbox_form #title", rootel).val(widgetData.title);
+                $("#dropbox_form #deadline", rootel).val(widgetData.deadline);                
             } else {
                 // Fill in dropbox defaults
-                
-            }
+                $("#dropbox_form #title", rootel).val("My Assignment Submission Box");
+                $("#dropbox_form #deadline", rootel).val(Date.now());                
+            }            
             $(dropboxDisplay, rootel).hide();
             $(dropboxSettings, rootel).show();
         };
@@ -170,7 +203,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 $.ajax({
                    url: dropboxWidgetPath, 
                    data: {
-                     "widgetId": tuid  
+                     "widgetid": tuid  
                    },
                    type: "GET",
                    dataType: "json",                   
@@ -192,7 +225,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                         }                       
                    },
                    error: function(jqXHR, textStatus, errorThrown) {
-                       alert("OH BOY - ERROR");
+                       alert("OH BOY - ERROR: " + jqXHR.responseText);
                    }
                 });                                
             } else {
