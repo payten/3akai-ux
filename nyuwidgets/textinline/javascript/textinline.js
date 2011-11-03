78ed6551f3986c86da5205ee4068ea1818c09f2a
Chris Roby <christopher.roby@gmail.com>
2011-11-03 15:40:34 -0700

    ATLASUI-376 Finishing up the text widget

--- nyuwidgets/textinline/javascript/textinline.js
+++ nyuwidgets/textinline/javascript/textinline.js
@@ -48,7 +48,7 @@ require(["jquery", "sakai/sakai.api.core", "jquery-plugins/jquery.jeditable"], f
             $textinline_save = $("#textinline_save", $rootel),
             $textinline_cancel = $("#textinline_cancel", $rootel);
 
-        widgetData = widgetData || {textinline:{title:"", text:""}};
+        widgetData = widgetData || {textinline:{title:"", text:"", allowTitleEdit: true}};
 
         var saveEdits = function(value, settings) {
             // The 'pre' is the textarea, otherwise it's the header (title)
@@ -57,13 +57,11 @@ require(["jquery", "sakai/sakai.api.core", "jquery-plugins/jquery.jeditable"], f
             } else {
                 widgetData.textinline.title = value;
             }
-            debug.log(widgetData.textinline);
             sakai.api.Widgets.saveWidgetData(tuid, widgetData.textinline);
             return value;
         };
 
         var doInit = function(){
-            debug.log("doInit");
             $textinline_main.html(sakai.api.Util.TemplateRenderer($textinline_main_template, {data:widgetData.textinline})).show();
             // Use a local widget data here for checking if the user can edit the container
             // if widgetData has no data property, it is our local widget data
@@ -72,8 +70,15 @@ require(["jquery", "sakai/sakai.api.core", "jquery-plugins/jquery.jeditable"], f
                 localwd = widgetData;
             }
             if (sakai.api.Widgets.canEditContainer(localwd, tuid)) {
-                $("h1.editable", $rootel).editable(saveEdits, {onblur: "submit"});
-                $("pre.editable", $rootel).editable(saveEdits, {type: "textarea", onblur: "submit", rows: 5});
+                var opts = {onblur: "ignore",submit: "<button class='s3d-button s3d-overlay-button'>Save</button>", cancel: "<button class='s3d-link-button'>Cancel</button>"};
+                if (widgetData.textinline.allowTitleEdit) {
+                    var h1opts = $.extend(true, {tooltip: "Click to edit title", placeholder: "Click to edit title"}, opts);
+                    $("h1.editable", $rootel).editable(saveEdits, h1opts);
+                } else {
+                    $("h1.editable").removeClass("editable");
+                }
+                var preOpts = $.extend(true, {type: "textarea", rows: 5, tooltip: "Click to edit text", placeholder: "Click to edit text"}, opts);
+                $("pre.editable", $rootel).editable(saveEdits, preOpts);
             } else {
                 $(".editable").removeClass("editable");
             }
