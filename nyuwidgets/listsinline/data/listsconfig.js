bd8e2ab623d477ff5f1a303e234b2c804a28013f
Chris Roby <christopher.roby@gmail.com>
2011-11-17 12:48:49 -0800

    ATLASUI-374 Lists widget

--- nyuwidgets/listsinline/data/listsconfig.js
+++ nyuwidgets/listsinline/data/listsconfig.js
@@ -3,417 +3,417 @@ define(function() {
     "program": {
         "title": "Program",
         "multiple": false,
-        "list": [
-            {
+        "list": {
+            "mpapnp": {
                 "title": "MPA: PNP",
                 "loc": "http://sakaiproject.org/product-overview"
             },
-            {
+            "mpahpam": {
                 "title": "MPA: HPAM",
                 "loc": "https://atlas.nyu.edu/~mpa--hpam"
             },
-            {
+            "mup": {
                 "title": "MUP",
                 "loc": "http://sakaiproject.org/community-overview"
             },
-            {
+            "empa": {
                 "title": "EMPA",
                 "loc": "http://sakaiproject.org/try-sakai-overview"
             },
-            {
+            "epmanurse": {
                 "title": "EMPA: Nurse Leaders",
                 "loc": "http://sakaiproject.org/news-events"
             },
-            {
+            "phd": {
                 "title": "PhD",
                 "loc": "http://sakaiproject.org/contact"
             }
-        ]
+        }
     },
     "specialization": {
         "title": "Specialization",
         "multiple": true,
-        "list": [
-            {
+        "list": {
+            "finance": {
                 "title": "Finance"
             },
-            {
+            "intl": {
                 "title": "International"
             },
-            {
+            "mgmt": {
                 "title": "Management"
             },
-            {
+            "policy": {
                 "title": "Policy"
             },
-            {
+            "envinftrans": {
                 "title": "Environment, Infrastructure, and Transportation"
             },
-            {
+            "econdev": {
                 "title": "Economic Development and Housing Policy"
             },
-            {
+            "intlplan": {
                 "title": "International Planning"
             }
-        ]
+        }
     },
     "enrollment": {
         "title": "Enrollment Status",
         "multiple": false,
-        "list": [
-            {
+        "list": {
+            "ft": {
                 "title": "Full-Time"
             },
-            {
+            "pt": {
                 "title": "Part-Time"
             }
-        ]
+        }
     },
     "graddate": {
         "title": "Graduation Date",
         "multiple": false,
-        "list": [
-            {
+        "list": {
+            "j11": {
                 "title": "January 2011"
             },
-            {
+            "m11": {
                 "title": "May 2011"
             },
-            {
+            "s11": {
                 "title": "September 2011"
             },
-            {
+            "j12": {
                 "title": "January 2012"
             },
-            {
+            "m12": {
                 "title": "May 2012"
             },
-            {
+            "s12": {
                 "title": "September 2012"
             },
-            {
+            "j13": {
                 "title": "January 2013"
             },
-            {
+            "m13": {
                 "title": "May 2013"
             },
-            {
+            "s13": {
                 "title": "September 2013"
             },
-            {
+            "j14": {
                 "title": "January 2014"
             },
-            {
+            "m14": {
                 "title": "May 2014"
             },
-            {
+            "s14": {
                 "title": "September 2014"
             },
-            {
+            "j15": {
                 "title": "January 2015"
             },
-            {
+            "m15": {
                 "title": "May 2015"
             },
-            {
+            "s15": {
                 "title": "September 2015"
             }
-        ]
+        }
     },
     "interests": {
         "title": "Interest Area(s)",
         "multiple": true,
         "newtab": true,
-        "list": [
-            {
+        "list": {
+            "aging": {
                 "title": "Aging",
                 "loc": "/search/groups#q=Aging&facet=all&filter=&page=1"
             },
-            {
+            "artsculture": {
                 "title": "Arts and Culture",
                 "loc": "/search/groups#q=Arts%2520and%2520Culture&facet=all&filter=&page=1"
             },
-            {
+            "childrenfamilies": {
                 "title": "Children and Families",
                 "loc": "/search/groups#q=Children%2520and%2520Families&facet=all&filter=&page=1"
             },
-            {
+            "civhumrts": {
                 "title": "Civil Rights/Human Rights",
                 "loc": "/search/groups#q=Civil%2520Rights%252FHuman%2520Rights&facet=all&filter=&page=1"
             },
-            {
+            "coalition": {
                 "title": "Coalition Building",
                 "loc": "/search/groups#q=Coalition%2520Building&facet=all&filter=&page=1"
             },
-            {
+            "commdev": {
                 "title": "Community Development",
                 "loc": "/search/groups#q=Community%2520Development&facet=all&filter=&page=1"
             },
-            {
+            "corpacct": {
                 "title": "Corporate Accountability",
                 "loc": "/search/groups#q=Corporate%2520Accountability&facet=all&filter=&page=1"
             },
-            {
+            "iddiv": {
                 "title": "Identity and Diversity",
                 "loc": "/search/groups#q=Identity%2520and%2520Diversity&facet=all&filter=&page=1"
             },
-            {
+            "econdev": {
                 "title": "Economic Development",
                 "loc": "/search/groups#q=Economic%2520Development&facet=all&filter=&page=1"
             },
-            {
+            "edu": {
                 "title": "Education",
                 "loc": "/search/groups#q=Education&facet=all&filter=&page=1"
             },
-            {
+            "elect": {
                 "title": "Elections",
                 "loc": "/search/groups#q=Elections&facet=all&filter=&page=1"
             },
-            {
+            "env": {
                 "title": "Environment",
                 "loc": "/search/groups#q=Environment&facet=all&filter=&page=1"
             },
-            {
+            "finance": {
                 "title": "Finance",
                 "loc": "/search/groups#q=Finance&facet=all&filter=&page=1"
             },
-            {
+            "food": {
                 "title": "Food & Nutrition",
                 "loc": "/search/groups#q=Food%2520%2526%2520Nutrition&facet=all&filter=&page=1"
             },
-            {
+            "globhealth": {
                 "title": "Global Health",
                 "loc": "/search/groups#q=Global%2520Health&facet=all&filter=&page=1"
             },
-            {
+            "govern": {
                 "title": "Governance",
                 "loc": "/search/groups#q=Governance&facet=all&filter=&page=1"
             },
-            {
+            "health": {
                 "title": "Health",
                 "loc": "/search/groups#q=Health&facet=all&filter=&page=1"
             },
-            {
+            "housing": {
                 "title": "Housing",
                 "loc": "/search/groups#q=Housing&facet=all&filter=&page=1"
             },
-            {
+            "immigration": {
                 "title": "Immigration",
                 "loc": "/search/groups#q=Immigration&facet=all&filter=&page=1"
             },
-            {
+            "infra": {
                 "title": "Infrastructure",
                 "loc": "/search/groups#q=Infrastructure&facet=all&filter=&page=1"
             },
-            {
+            "intldev": {
                 "title": "International Development",
                 "loc": "/search/groups#q=International%2520Development&facet=all&filter=&page=1"
             },
-            {
+            "justice": {
                 "title": "Justice System",
                 "loc": "/search/groups#q=Justice%2520System&facet=all&filter=&page=1"
             },
-            {
+            "labor": {
                 "title": "Labor and Employment",
                 "loc": "/search/groups#q=Labor%2520and%2520Employment&facet=all&filter=&page=1"
             },
-            {
+            "leadership": {
                 "title": "Leadership Development",
                 "loc": "/search/groups#q=Leadership%2520Development&facet=all&filter=&page=1"
             },
-            {
+            "localstategovt": {
                 "title": "Local and/or State Government",
                 "loc": "/search/groups#q=Local%2520and%252For%2520State%2520Government&facet=all&filter=&page=1"
             },
-            {
+            "microfinance": {
                 "title": "Microfinance",
                 "loc": "/search/groups#q=Microfinance&facet=all&filter=&page=1"
             },
-            {
+            "npmanage": {
                 "title": "Nonprofit Management",
                 "loc": "/search/groups#q=Nonprofit%2520Management&facet=all&filter=&page=1"
             },
-            {
+            "nyugov": {
                 "title": "NYC Government",
                 "loc": "/search/groups#q=NYC%2520Government&facet=all&filter=&page=1"
             },
-            {
+            "orgdev": {
                 "title": "Organizational Development",
                 "loc": "/search/groups#q=Organizational%2520Development&facet=all&filter=&page=1"
             },
-            {
+            "philan": {
                 "title": "Philanthropy",
                 "loc": "/search/groups#q=Philanthropy&facet=all&filter=&page=1"
             },
-            {
+            "politix": {
                 "title": "Politics",
                 "loc": "/search/groups#q=Politics&facet=all&filter=&page=1"
             },
-            {
+            "povhung": {
                 "title": "Poverty/Hunger",
                 "loc": "/search/groups#q=q=Poverty%252FHunger&facet=all&filter=&page=1"
             },
-            {
+            "pubpol": {
                 "title": "Public Policy",
                 "loc": "/search/groups#q=Public%2520Policy&facet=all&filter=&page=1"
             },
-            {
+            "rafrican": {
                 "title": "Region - Africa: North",
                 "loc": "/search/groups#q=Region%2520-%2520Africa%253A%2520North&facet=all&filter=&page=1"
             },
-            {
+            "rafricasub": {
                 "title": "Region - Africa: Sub-Saharan",
                 "loc": "/search/groups#q=Region%2520-%2520Africa%253A%2520Sub-Saharan&facet=all&filter=&page=1"
             },
-            {
+            "rasiacent": {
                 "title": "Region - Asia: Central",
                 "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520Central&facet=all&filter=&page=1"
             },
-            {
+            "rasiae": {
                 "title": "Region - Asia: East",
                 "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520East&facet=all&filter=&page=1"
             },
-            {
+            "rasias": {
                 "title": "Region - Asia: South",
                 "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520South&facet=all&filter=&page=1"
             },
-            {
+            "rasiase": {
                 "title": "Region - Asia: Southeast",
                 "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520Southeast&facet=all&filter=&page=1"
             },
-            {
+            "rcentam": {
                 "title": "Region - Central America",
                 "loc": "/search/groups#q=Region%2520-%2520Central America&facet=all&filter=&page=1"
             },
-            {
+            "reue": {
                 "title": "Region - Europe: Eastern",
                 "loc": "/search/groups#q=Region%2520-%2520Europe%253A%2520Eastern&facet=all&filter=&page=1"
             },
-            {
+            "reuw": {
                 "title": "Region - Europe: Western",
                 "loc": "/search/groups#q=Region%2520-%2520Europe%253A%2520Western&facet=all&filter=&page=1"
             },
-            {
+            "russr": {
                 "title": "Region - Former Soviet Republics",
                 "loc": "/search/groups#q=Region%2520-%2520Former Soviet Republics&facet=all&filter=&page=1"
             },
-            {
+            "rme": {
                 "title": "Region - Middle East",
                 "loc": "/search/groups#q=Region%2520-%2520Middle East&facet=all&filter=&page=1"
             },
-            {
+            "rna": {
                 "title": "Region - North America",
                 "loc": "/search/groups#q=Region%2520-%2520North America&facet=all&filter=&page=1"
             },
-            {
+            "rr": {
                 "title": "Region - Russia",
                 "loc": "/search/groups#q=Region%2520-%2520Russia&facet=all&filter=&page=1"
             },
-            {
+            "rsam": {
                 "title": "Region - South America",
                 "loc": "/search/groups#q=Region%2520-%2520South America&facet=all&filter=&page=1"
             },
-            {
+            "rus": {
                 "title": "Region - United States",
                 "loc": "/search/groups#q=Region%2520-%2520United States&facet=all&filter=&page=1"
             },
-            {
+            "sociale": {
                 "title": "Social Entrepreneurship",
                 "loc": "/search/groups#q=Social%2520Entrepreneurship&facet=all&filter=&page=1"
             },
-            {
+            "transpo": {
                 "title": "Transportation",
                 "loc": "/search/groups#q=Transportation&facet=all&filter=&page=1"
             },
-            {
+            "urbp": {
                 "title": "Urban Planning",
                 "loc": "/search/groups#q=Urban%2520Planning&facet=all&filter=&page=1"
             },
-            {
+            "usg": {
                 "title": "US Federal Government",
                 "loc": "/search/groups#q=US%2520Federal%2520Government&facet=all&filter=&page=1"
             },
-            {
+            "usnp": {
                 "title": "US National Politics",
                 "loc": "/search/groups#q=US%2520National%2520Politics&facet=all&filter=&page=1"
             },
-            {
+            "water": {
                 "title": "Water",
                 "loc": "/search/groups#q=Water&facet=all&filter=&page=1"
             }
-        ]
+        }
     },
     "studentgroups": {
         "title": "Student Groups",
         "multiple": true,
-        "list": [
-            {
+        "list": {
+            "wsa": {
                 "title": "Wagner Student Association (WSA)"
             },
-            {
+            "alas": {
                 "title":"Alliance of Latin American Students (ALAS)"
             },
-            {
+            "apasa": {
                 "title": "Asian Pacific American Student Alliance (APASA)"
             },
-            {
+            "bsa": {
                 "title": "Black Student Alliance (BSA)"
             },
-            {
+            "jewpa": {
                 "title": "Jewish Wagner Professional Association (JeWPA)"
             },
-            {
+            "npn": {
                 "title": "Non Profit Network (NPN)"
             },
-            {
+            "spa": {
                 "title": "Stonewall Policy Alliance (SPA)"
             },
-            {
+            "scjr": {
                 "title": "Students for Criminal Justice Reform (SCJR)"
             },
-            {
+            "sneac": {
                 "title": "Students Exploring Arts and Culture (SNEAC)"
             },
-            {
+            "bridge": {
                 "title": "Students for Innovation (BRIDGE)"
             },
-            {
+            "uspa": {
                 "title": "Urban Planning Student Association (UPSA)"
             },
-            {
+            "wagrev": {
                 "title": "Wagner Review"
             },
-            {
+            "wefa": {
                 "title": "Wagner Economic and Finance Association (WEFA)"
             },
-            {
+            "wepa": {
                 "title": "Wagner Environmental Policy Association (WEPA)"
             },
-            {
+            "wespa": {
                 "title": "Wagner Education Policy Studies Association (WEPSA)"
             },
-            {
+            "whn": {
                 "title": "Wagner Health Network (WHN)"
             },
-            {
+            "wiss": {
                 "title": "Wagner International Student Society (WISS)"
             },
-            {
+            "wpa": {
                 "title": "Wagner Policy Alliance (WPA)"
             },
-            {
-                "title": "Wagner Student Association for Africa(WSAFA)"
+            "wsafa": {
+                "title": "Wagner Student Association for Africa (WSAFA)"
             },
-            {
+            "wta": {
                 "title": "Wagner Transportation Association (WTA)"
             },
-            {
+            "wvc": {
                 "title": "Wagner Volunteer Corps (WVC)"
             },
-            {
+            "wwc": {
                 "title": "Wagner Women's Caucus (WWC)"
             }
-        ]
+        }
     }
 };
 });
--- nyuwidgets/listsinline/javascript/listsinline.js
+++ nyuwidgets/listsinline/javascript/listsinline.js
@@ -4,17 +4,21 @@ require(["jquery", "sakai/sakai.api.core", "/nyuwidgets/listsinline/data/listsco
 
         // Dom identifiers
         var $rootel = $("#" + tuid),
+            $listsinline_main_container = $("#listsinline_main_container", $rootel),
+            $listsinline_title = $("#listsinline_title h1", $rootel),
             $listsinline_display_lists = $("#listsinline_display_lists", $rootel),
             $listsinline_display_lists_template = $("#listsinline_display_lists_template", $rootel),
             $listsinline_edit_button = $("#listsinline_edit_button", $rootel),
             $listsinline_choose_values = $("#listsinline_choose_values", $rootel),
             $listsinline_choose_values_template = $("#listsinline_choose_values_template", $rootel),
+            $listsinline_choose_values_form = $("#listsinline_choose_values form", $rootel),
             $listsinline_settings = $("#listsinline_settings", $rootel),
             $listsinline_settings_template = $("#listsinline_settings_template", $rootel),
             $listsinline_settings_form = $("#listsinline_settings form");
 
         var thisList = false,
-            canEdit = false;
+            canEdit = false,
+            thisData = {};
 
         var getCategories = function() {
             var categories = [];
@@ -24,6 +28,11 @@ require(["jquery", "sakai/sakai.api.core", "/nyuwidgets/listsinline/data/listsco
             return categories;
         };
 
+        var getChoiceObject = function(choiceKey) {
+            var listObj = thisList[thisData.category];
+            return listObj.list[choiceKey];
+        };
+
         var showViewInterface = function() {
             $listsinline_choose_values.hide();
             // show the edit button if the user can edit the page
@@ -31,8 +40,8 @@ require(["jquery", "sakai/sakai.api.core", "/nyuwidgets/listsinline/data/listsco
                 $listsinline_edit_button.show();
             }
             // Render the widget data choices
-            if (widgetData.choices) {
-                sakai.api.Util.TemplateRenderer($listsinline_display_lists_template, {choices: widgetData.choices}, $listsinline_display_lists);
+            if (thisData.choicesObjects) {
+                sakai.api.Util.TemplateRenderer($listsinline_display_lists_template, {choices: thisData.choicesObjects}, $listsinline_display_lists);
                 $listsinline_display_lists.show();
             } else {
                 // maybe show an error message or something
@@ -41,8 +50,9 @@ require(["jquery", "sakai/sakai.api.core", "/nyuwidgets/listsinline/data/listsco
 
         var showEditInterface = function() {
             $listsinline_display_lists.hide();
+            $listsinline_edit_button.hide();
             // grab the current widgetData.category and render all the choices
-            var data = {list: thisList[widgetData.category]};
+            var data = {list: thisList[thisData.category], choices: thisData.choices || []};
             sakai.api.Util.TemplateRenderer($listsinline_choose_values_template, data, $listsinline_choose_values);
             $listsinline_choose_values.show();
         };
@@ -50,39 +60,70 @@ require(["jquery", "sakai/sakai.api.core", "/nyuwidgets/listsinline/data/listsco
         var showSettingsInterface = function() {
             var categories = getCategories();
             // render choices in a dropdown
-            sakai.api.Util.TemplateRenderer($listsinline_settings_template, categories, $listsinline_settings);
+            sakai.api.Util.TemplateRenderer($listsinline_settings_template, {categories: categories, selected: thisData.category}, $listsinline_settings);
             $listsinline_settings.show();
         };
 
-
         var saveChoices = function(e) {
-            // serialize the form
-            // save it in widgetData.choices
-            // show the view interface again
-            showViewInterface();
-            e.preventDefault();
+            // clear out all the data we're going to be saving
+            thisData.choices = [];
+            thisData.choicesSearchable = "";
+            thisData.choicesObjects = [];
+            if (thisList[thisData.category].multiple) {
+                $(e.target).find("input:checked").each(function(i,elt) {
+                    thisData.choices.push($(elt).val());
+                    thisData.choicesObjects.push(getChoiceObject($(elt).val()));
+                    thisData.choicesSearchable += $(elt).next("label").text() + ", ";
+                });
+            } else {
+                var $selection = $(e.target).find("option:selected");
+                thisData.choices.push($selection.val());
+                thisData.choicesObjects.push(getChoiceObject($selection.val()));
+                thisData.choicesSearchable += $selection.text() + ", ";
+            }
+            sakai.api.Widgets.saveWidgetData(tuid, {listsinline: thisData}, function(success, data) {
+                showViewInterface();
+            });
+            return false;
         };
 
         var saveSettings = function(e) {
             // get the value from the dropdown and save it
             // inform finish of widget settings
-            e.preventDefault();
+            var selection = $(e.target).find("select").val();
+            thisData.category = selection;
+            sakai.api.Widgets.saveWidgetData(tuid, {listsinline: {category: selection}}, function(success, data){
+                sakai.api.Widgets.Container.informFinish(tuid, "listsinline");
+            }, true);
+            return false;
         };
 
         var addBindings = function() {
             // bind to the save button in the settings form
-            $listsinline_settings_form.live("submit", saveSettings);
+            $listsinline_settings_form.die("submit").live("submit", saveSettings);
             // bind to the edit button in the view interface
-            $listsinline_edit_button.live("click", showEditInterface);
+            $listsinline_edit_button.die("click").live("click", showEditInterface);
+            $listsinline_choose_values_form.die("submit").live("submit", saveChoices);
         };
 
         var doInit = function(){
+            addBindings();
             thisList = $.extend(true, {}, listconfig);
-            // if (showSettings) {
-                showSettingsInterface();
-            // } else {
-            //     showViewInterface();
-            // }
+            sakai.api.Widgets.loadWidgetData(tuid, function(success, data) {
+                if (success && data && data.listsinline) {
+                    debug.log(data);
+                    thisData = data.listsinline;
+                }
+                if (showSettings) {
+                    showSettingsInterface();
+                } else {
+                    if (thisData.category) {
+                        $listsinline_title.text(thisList[thisData.category].title);
+                    }
+                    $listsinline_main_container.show();
+                    showViewInterface();
+                }
+            });
         };
         doInit();
     };
