006374f2e28354560e4d959f536a5d138d1fada7
andrewsears <andrew@andrewsears.com>
2011-12-12 16:51:29 -0500

    Updated the right URL for the Gradebook

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -978,7 +978,7 @@ define(["config/config"], function(config) {
         "pseudogroup": "g-gradecenter"
     });
     config.Navigation[0]["subnav"].push({
-        "url": "http://www.yahoo.edu",
+        "url": "https://admin.portal.nyu.edu/psp/paprod/EMPLOYEE/EMPL/h/?tab",
         "id": "subnavigation_gradecenter_link",
         "label": "MY_GRADECENTER",
         "pseudogroup": "g-gradecenter",
