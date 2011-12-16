278a043eb27b50f446850c70128b95e442c30825
Chris Roby <christopher.roby@gmail.com>
2011-12-15 22:31:22 -0800

    ATLASUI-444 Final fixes for the merge

--- nyuwidgets/listsinline/data/listsconfig.js
+++ nyuwidgets/listsinline/data/listsconfig.js
@@ -127,219 +127,219 @@ define(function() {
         "list": {
             "aging": {
                 "title": "Aging",
-                "loc": "/search/groups#q=Aging&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Aging"
             },
             "artsculture": {
                 "title": "Arts and Culture",
-                "loc": "/search/groups#q=Arts%2520and%2520Culture&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Arts%2520and%2520Culture"
             },
             "childrenfamilies": {
                 "title": "Children and Families",
-                "loc": "/search/groups#q=Children%2520and%2520Families&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Children%2520and%2520Families"
             },
             "civhumrts": {
                 "title": "Civil Rights/Human Rights",
-                "loc": "/search/groups#q=Civil%2520Rights%252FHuman%2520Rights&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Civil%2520Rights%252FHuman%2520Rights"
             },
             "coalition": {
                 "title": "Coalition Building",
-                "loc": "/search/groups#q=Coalition%2520Building&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Coalition%2520Building"
             },
             "commdev": {
                 "title": "Community Development",
-                "loc": "/search/groups#q=Community%2520Development&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Community%2520Development"
             },
             "corpacct": {
                 "title": "Corporate Accountability",
-                "loc": "/search/groups#q=Corporate%2520Accountability&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Corporate%2520Accountability"
             },
             "iddiv": {
                 "title": "Identity and Diversity",
-                "loc": "/search/groups#q=Identity%2520and%2520Diversity&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Identity%2520and%2520Diversity"
             },
             "econdev": {
                 "title": "Economic Development",
-                "loc": "/search/groups#q=Economic%2520Development&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Economic%2520Development"
             },
             "edu": {
                 "title": "Education",
-                "loc": "/search/groups#q=Education&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Education"
             },
             "elect": {
                 "title": "Elections",
-                "loc": "/search/groups#q=Elections&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Elections"
             },
             "env": {
                 "title": "Environment",
-                "loc": "/search/groups#q=Environment&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Environment"
             },
             "finance": {
                 "title": "Finance",
-                "loc": "/search/groups#q=Finance&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Finance"
             },
             "food": {
                 "title": "Food & Nutrition",
-                "loc": "/search/groups#q=Food%2520%2526%2520Nutrition&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Food%2520%2526%2520Nutrition"
             },
             "globhealth": {
                 "title": "Global Health",
-                "loc": "/search/groups#q=Global%2520Health&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Global%2520Health"
             },
             "govern": {
                 "title": "Governance",
-                "loc": "/search/groups#q=Governance&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Governance"
             },
             "health": {
                 "title": "Health",
-                "loc": "/search/groups#q=Health&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Health"
             },
             "housing": {
                 "title": "Housing",
-                "loc": "/search/groups#q=Housing&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Housing"
             },
             "immigration": {
                 "title": "Immigration",
-                "loc": "/search/groups#q=Immigration&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Immigration"
             },
             "infra": {
                 "title": "Infrastructure",
-                "loc": "/search/groups#q=Infrastructure&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Infrastructure"
             },
             "intldev": {
                 "title": "International Development",
-                "loc": "/search/groups#q=International%2520Development&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=International%2520Development"
             },
             "justice": {
                 "title": "Justice System",
-                "loc": "/search/groups#q=Justice%2520System&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Justice%2520System"
             },
             "labor": {
                 "title": "Labor and Employment",
-                "loc": "/search/groups#q=Labor%2520and%2520Employment&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Labor%2520and%2520Employment"
             },
             "leadership": {
                 "title": "Leadership Development",
-                "loc": "/search/groups#q=Leadership%2520Development&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Leadership%2520Development"
             },
             "localstategovt": {
                 "title": "Local and/or State Government",
-                "loc": "/search/groups#q=Local%2520and%252For%2520State%2520Government&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Local%2520and%252For%2520State%2520Government"
             },
             "microfinance": {
                 "title": "Microfinance",
-                "loc": "/search/groups#q=Microfinance&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Microfinance"
             },
             "npmanage": {
                 "title": "Nonprofit Management",
-                "loc": "/search/groups#q=Nonprofit%2520Management&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Nonprofit%2520Management"
             },
             "nyugov": {
                 "title": "NYC Government",
-                "loc": "/search/groups#q=NYC%2520Government&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=NYC%2520Government"
             },
             "orgdev": {
                 "title": "Organizational Development",
-                "loc": "/search/groups#q=Organizational%2520Development&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Organizational%2520Development"
             },
             "philan": {
                 "title": "Philanthropy",
-                "loc": "/search/groups#q=Philanthropy&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Philanthropy"
             },
             "politix": {
                 "title": "Politics",
-                "loc": "/search/groups#q=Politics&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Politics"
             },
             "povhung": {
                 "title": "Poverty/Hunger",
-                "loc": "/search/groups#q=q=Poverty%252FHunger&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=q=Poverty%252FHunger"
             },
             "pubpol": {
                 "title": "Public Policy",
-                "loc": "/search/groups#q=Public%2520Policy&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Public%2520Policy"
             },
             "rafrican": {
                 "title": "Region - Africa: North",
-                "loc": "/search/groups#q=Region%2520-%2520Africa%253A%2520North&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Africa%253A%2520North"
             },
             "rafricasub": {
                 "title": "Region - Africa: Sub-Saharan",
-                "loc": "/search/groups#q=Region%2520-%2520Africa%253A%2520Sub-Saharan&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Africa%253A%2520Sub-Saharan"
             },
             "rasiacent": {
                 "title": "Region - Asia: Central",
-                "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520Central&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Asia%253A%2520Central"
             },
             "rasiae": {
                 "title": "Region - Asia: East",
-                "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520East&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Asia%253A%2520East"
             },
             "rasias": {
                 "title": "Region - Asia: South",
-                "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520South&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Asia%253A%2520South"
             },
             "rasiase": {
                 "title": "Region - Asia: Southeast",
-                "loc": "/search/groups#q=Region%2520-%2520Asia%253A%2520Southeast&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Asia%253A%2520Southeast"
             },
             "rcentam": {
                 "title": "Region - Central America",
-                "loc": "/search/groups#q=Region%2520-%2520Central America&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Central America"
             },
             "reue": {
                 "title": "Region - Europe: Eastern",
-                "loc": "/search/groups#q=Region%2520-%2520Europe%253A%2520Eastern&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Europe%253A%2520Eastern"
             },
             "reuw": {
                 "title": "Region - Europe: Western",
-                "loc": "/search/groups#q=Region%2520-%2520Europe%253A%2520Western&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Europe%253A%2520Western"
             },
             "russr": {
                 "title": "Region - Former Soviet Republics",
-                "loc": "/search/groups#q=Region%2520-%2520Former Soviet Republics&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Former Soviet Republics"
             },
             "rme": {
                 "title": "Region - Middle East",
-                "loc": "/search/groups#q=Region%2520-%2520Middle East&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Middle East"
             },
             "rna": {
                 "title": "Region - North America",
-                "loc": "/search/groups#q=Region%2520-%2520North America&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520North America"
             },
             "rr": {
                 "title": "Region - Russia",
-                "loc": "/search/groups#q=Region%2520-%2520Russia&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520Russia"
             },
             "rsam": {
                 "title": "Region - South America",
-                "loc": "/search/groups#q=Region%2520-%2520South America&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520South America"
             },
             "rus": {
                 "title": "Region - United States",
-                "loc": "/search/groups#q=Region%2520-%2520United States&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Region%2520-%2520United States"
             },
             "sociale": {
                 "title": "Social Entrepreneurship",
-                "loc": "/search/groups#q=Social%2520Entrepreneurship&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Social%2520Entrepreneurship"
             },
             "transpo": {
                 "title": "Transportation",
-                "loc": "/search/groups#q=Transportation&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Transportation"
             },
             "urbp": {
                 "title": "Urban Planning",
-                "loc": "/search/groups#q=Urban%2520Planning&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Urban%2520Planning"
             },
             "usg": {
                 "title": "US Federal Government",
-                "loc": "/search/groups#q=US%2520Federal%2520Government&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=US%2520Federal%2520Government"
             },
             "usnp": {
                 "title": "US National Politics",
-                "loc": "/search/groups#q=US%2520National%2520Politics&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=US%2520National%2520Politics"
             },
             "water": {
                 "title": "Water",
-                "loc": "/search/groups#q=Water&facet=all&filter=&page=1"
+                "loc": "/search#l=portfolio&q=Water"
             }
         }
     },
