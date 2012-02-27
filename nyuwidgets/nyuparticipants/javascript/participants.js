8c4aacc78b49e447eddab0898e993ba61fd75235
Payten Giles <payten.giles@gmail.com>
2012-02-27 12:19:34 +1100

    clean up code comments for merge

--- nyuwidgets/nyuparticipants/javascript/participants.js
+++ nyuwidgets/nyuparticipants/javascript/participants.js
@@ -39,8 +39,8 @@ require(["jquery", "sakai/sakai.api.core", "/dev/javascript/search_util.js"], fu
 
         var showExtraInfo = false;
         var showTagCloud = false;
-        // if legacy participants widget (< v1.1)...
-        if (widgetData.hasOwnProperty("participants")) { // if legacy participants widget (< v1.1)...
+        // if legacy participants widget (prior to v1.1)...
+        if (widgetData.hasOwnProperty("participants")) {
             showExtraInfo = widgetData.participants.showExtraInfo;
         } else if (widgetData.hasOwnProperty("nyuparticipants")) { // new > v1.1 widget
             showExtraInfo = widgetData.nyuparticipants.showExtraInfo;
