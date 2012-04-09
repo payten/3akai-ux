fb3a5010fe536b4904860fe5729f89b414159aae
Payten Giles <payten.giles@gmail.com>
2012-04-09 21:17:01 +1000

    NOJIRA update bundle string key names

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -259,8 +259,8 @@ define(["config/config"], function(config) {
      * Add our custom user areas
      */
     config.defaultpubstructure["structure0"]["wall"] = {
-        "_title": "__MSG__USER_DASHBOARD_TITLE__",
-        "_altTitle": "__MSG__USER_DASHBOARD_ALT_TITLE__",
+        "_title": "__MSG__USER_PUBLIC_DASHBOARD_TITLE__",
+        "_altTitle": "__MSG__USER_PUBLIC_DASHBOARD_ALT_TITLE__",
         "_order": -1,
         "_view": "anonymous",
         "_canEdit": true,
@@ -270,7 +270,7 @@ define(["config/config"], function(config) {
         "main": {
             "_ref": "${refid}3",
             "_order": 0,
-            "_title": "__MSG__USER_DASHBOARD_TITLE__"
+            "_title": "__MSG__USER_PUBLIC_DASHBOARD_TITLE__"
         }
     };
     config.defaultpubstructure["${refid}3"] = {
@@ -279,7 +279,7 @@ define(["config/config"], function(config) {
                 "a-tuid='${refid}4'>__MSG__EDIT_LAYOUT__</button><button type='button' class='s3d-button "+
                 "s3d-margin-top-5 s3d-header-button s3d-header-smaller-button dashboard_global_a"+
                 "dd_widget' data-tuid='${refid}4'>__MSG__ADD_WIDGET__</button></div><div class='s3d-conte"+
-                "ntpage-title'>__MSG__USER_DASHBOARD_TITLE__</div><div id='widget_dashboard_${refid}4' class='widget_inline'></div>"    
+                "ntpage-title'>__MSG__USER_PUBLIC_DASHBOARD_TITLE__</div><div id='widget_dashboard_${refid}4' class='widget_inline'></div>"    
     };
     config.defaultpubstructure["${refid}4"] = {
         "dashboard": {
