6b1f8d86a7906599a0b5fb12707a3d39c78ecef5
Payten Giles <payten.giles@gmail.com>
2012-05-04 08:40:56 +1000

    NOJIRA disable creation of My wall for newly created users and remove from Me menu

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -258,43 +258,6 @@ define(["config/config"], function(config) {
 
 
     /**
-     * Add our custom user areas
-     */
-    config.defaultpubstructure["structure0"]["wall"] = {
-        "_title": "__MSG__USER_PUBLIC_DASHBOARD_TITLE__",
-        "_altTitle": "__MSG__USER_PUBLIC_DASHBOARD_ALT_TITLE__",
-        "_order": -1,
-        "_view": "private",
-        "_canEdit": true,
-        "_reorderOnly": false,
-        "_nonEditable": true,
-        "_ref": "${refid}3",
-        "main": {
-            "_ref": "${refid}3",
-            "_order": 0,
-            "_title": "__MSG__USER_PUBLIC_DASHBOARD_TITLE__"
-        }
-    };
-    config.defaultpubstructure["${refid}3"] = {
-        "page": "<div class='fl-force-right dashboard-admin-actions' style='display:none;'><button type='button' class='s3d-button s3d-margin"+
-                "-top-5 s3d-header-button s3d-header-smaller-button dashboard_change_layout' dat"+
-                "a-tuid='${refid}4'>__MSG__EDIT_LAYOUT__</button><button type='button' class='s3d-button "+
-                "s3d-margin-top-5 s3d-header-button s3d-header-smaller-button dashboard_global_a"+
-                "dd_widget' data-tuid='${refid}4'>__MSG__ADD_WIDGET__</button></div><div class='s3d-conte"+
-                "ntpage-title'>__MSG__USER_PUBLIC_DASHBOARD_TITLE__</div><div id='widget_dashboard_${refid}4' class='widget_inline'></div>"    
-    };
-    config.defaultpubstructure["${refid}4"] = {
-        "dashboard": {
-            "layout": "threecolumn",
-            "columns": {
-                "column1": [],
-                "column2": [],
-                "column3": []
-            }
-        }
-    }
-
-    /**
      * Authentication
      */
     config.Authentication.internal = true;
@@ -367,11 +330,6 @@ define(["config/config"], function(config) {
     config.Navigation[0]["subnav"].push({
         "id": "subnavigation_hr",
         "pseudogroup": "g-gradecenter"
-    });
-    config.Navigation[0]["subnav"].splice(3,0, {
-        "url": "/me#l=wall",
-        "id": "subnavigation_mywall_link",
-        "label": "USER_PUBLIC_DASHBOARD_TITLE"
     });
 
     /**
